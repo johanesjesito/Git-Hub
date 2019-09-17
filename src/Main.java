3950f9be5b807d2e48421598315f7c9edd75daaa
Johanes <Aluno@Aluno-PC>
2019-09-17 20:18:26 -0300

    usuario feito, e salvo pelo main e exibido

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Usuario usuario = new Usuario("joao", "1234");
+		
+		System.out.println(usuario.getLogin());
+		System.out.println(usuario.getSenha());
+	}
+	
+}
